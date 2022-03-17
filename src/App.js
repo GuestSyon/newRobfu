93f6622dcc5fdff1bb5218c017ab2519c87928a8
waldo-dev <waldojavier.vo@gmail.com>
2022-03-17 11:49:28 -0300

    cambios de interfaz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Home from "./views/Home";
+import { Router } from "@reach/router";
+import Three from "./components/Three";
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Home path="/" />
+        <Three path="/three" />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
